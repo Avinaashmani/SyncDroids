7b12aef3a2da197f620d65f11560d4e9f555c7d4
avinaash <avinaash.mani@gmail.com>
2025-03-21 17:45:00 +0530

    switched ROS2 version to Humble

--- build/droid_navigation/prefix_override/sitecustomize.py
+++ build/droid_navigation/prefix_override/sitecustomize.py
@@ -1,3 +1,4 @@
 import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/avinaash/SyncDroids/install/droid_navigation'
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/avinaash/SyncDroids/install/droid_navigation'
